556158c4f5b15a5e4c9f520a859a69123e6b4ac0
Mkprajapat95210 <mukesh9587826831@gmail.com>
2025-08-15 22:48:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import AboutPage from './pages/AboutPage.tsx';
-import About-threePage from './pages/About-threePage.tsx';
-import All-productPage from './pages/All-productPage.tsx';
+import AboutThreePage from './pages/About-threePage.tsx';
+import AllProductPage from './pages/All-productPage.tsx';
 import BlogPage from './pages/BlogPage.tsx';
-import Blog-detailsPage from './pages/Blog-detailsPage.tsx';
-import Blog-details-sidebarPage from './pages/Blog-details-sidebarPage.tsx';
+import BlogDetailsPage from './pages/Blog-detailsPage.tsx';
+import BlogDetailsSidebarPage from './pages/Blog-details-sidebarPage.tsx';
 import CartPage from './pages/CartPage.tsx';
-import Cart-paymentPage from './pages/Cart-paymentPage.tsx';
-import Cart-personalPage from './pages/Cart-personalPage.tsx';
-import Cart-thank-youPage from './pages/Cart-thank-youPage.tsx';
+import CartPaymentPage from './pages/Cart-paymentPage.tsx';
+import CartPersonalPage from './pages/Cart-personalPage.tsx';
+import CartThankYouPage from './pages/Cart-thank-youPage.tsx';
 import ContactPage from './pages/ContactPage.tsx';
 import DashboardPage from './pages/DashboardPage.tsx';
-import Dashboard-profilePage from './pages/Dashboard-profilePage.tsx';
+import DashboardProfilePage from './pages/Dashboard-profilePage.tsx';
 import DownloadPage from './pages/DownloadPage.tsx';
 import EarningPage from './pages/EarningPage.tsx';
 import FollowerPage from './pages/FollowerPage.tsx';
 import FollowingPage from './pages/FollowingPage.tsx';
 import IndexPage from './pages/IndexPage.tsx';
-import Index-2Page from './pages/Index-2Page.tsx';
-import Index-threePage from './pages/Index-threePage.tsx';
-import Index-twoPage from './pages/Index-twoPage.tsx';
+import Index2Page from './pages/Index-2Page.tsx';
+import IndexThreePage from './pages/Index-threePage.tsx';
+import IndexTwoPage from './pages/Index-twoPage.tsx';
 import LoginPage from './pages/LoginPage.tsx';
-import Product-detailsPage from './pages/Product-detailsPage.tsx';
-import Product-detaisPage from './pages/Product-detaisPage.tsx';
+import ProductDetailsPage from './pages/Product-detailsPage.tsx';
+import ProductDetaisPage from './pages/Product-detaisPage.tsx';
 import ProfilePage from './pages/ProfilePage.tsx';
 import RefundPage from './pages/RefundPage.tsx';
 import RegisterPage from './pages/RegisterPage.tsx';
@@ -33,31 +33,30 @@ import StatementPage from './pages/StatementPage.tsx';
 function App() {
   return (
     <div>
-      {/* You can add routing here to display different pages */}
       <AboutPage />
-      <About-threePage />
-      <All-productPage />
+      <AboutThreePage />
+      <AllProductPage />
       <BlogPage />
-      <Blog-detailsPage />
-      <Blog-details-sidebarPage />
+      <BlogDetailsPage />
+      <BlogDetailsSidebarPage />
       <CartPage />
-      <Cart-paymentPage />
-      <Cart-personalPage />
-      <Cart-thank-youPage />
+      <CartPaymentPage />
+      <CartPersonalPage />
+      <CartThankYouPage />
       <ContactPage />
       <DashboardPage />
-      <Dashboard-profilePage />
+      <DashboardProfilePage />
       <DownloadPage />
       <EarningPage />
       <FollowerPage />
       <FollowingPage />
       <IndexPage />
-      <Index-2Page />
-      <Index-threePage />
-      <Index-twoPage />
+      <Index2Page />
+      <IndexThreePage />
+      <IndexTwoPage />
       <LoginPage />
-      <Product-detailsPage />
-      <Product-detaisPage />
+      <ProductDetailsPage />
+      <ProductDetaisPage />
       <ProfilePage />
       <RefundPage />
       <RegisterPage />
